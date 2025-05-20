069f52ab14b3bf21253bf7695be347268bdbe875
tanatoyt <tanatoyt@gmail.com>
2025-05-20 15:34:41 +0000

    sync pro vscode

--- Doctor-hu-hu/src/components/screens/Adicionar/Adicionar.tsx
+++ Doctor-hu-hu/src/components/screens/Adicionar/Adicionar.tsx
@@ -1,17 +1,81 @@
-import Header from "@/components/Header/Header";
-import { View,Text, KeyboardAvoidingView } from "react-native";
-import { styles } from "./style";
-export default function Adicionar() {
-  return (
-    <>
-    <Header />
-    <KeyboardAvoidingView style={styles.fundo}>
-     
-      <View style={styles.container}>
+import React, {useState} from 'react';
+import { KeyboardAvoidingView, Platform, ScrollView, View } from 'react-native';
 
+import { styles } from './style';
+import Toast from 'react-native-toast-message';
+import { v4 as uuidv4 } from 'uuid';
+import AsyncStorage, { useAsyncStorage } from '@react-native-async-storage/async-storage';
+import { Input } from '@/components/Input/Input';
+import  { Button}  from '@/components/Button/Button';
+import { HeaderForm } from '@/components/HeaderForm/HeaderForm';
+
+
+export default function Adicionar() {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { getItem, setItem } = useAsyncStorage("@savepass:passwords");
+  
+  async function handleNew(){
+
+    const id = uuidv4();
+    const newData = {
+      id,
+      name,
+      description,
+      password
+
+    }
+    const response = await getItem();
+    const previousData = response ? JSON.parse(response) : [];
+
+    const data = [...previousData, newData];
+
+    await setItem(JSON.stringify(data));
+    Toast.show({
+      type: "success",
+      text1: "Cadastrado com sucesso!"
+    })
+  }
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={styles.container}
+    >
+      <View style={styles.content}>
+        <ScrollView>
+
+          <HeaderForm />
+
+          <View style={styles.form}>
+            <Input
+              label="Nome do remédio"
+              onChangeText={setName}
+              value={name}
+              autoCapitalize="none"
+            />
+            <Input
+              label="Descrição do remédio"
+              onChangeText={setDescription}
+              value={description}
+              autoCapitalize="none"
+            />
+            <Input
+              label="Senha"
+              secureTextEntry
+              onChangeText={setPassword}
+            />
+          </View>
+
+          <View style={styles.footer}>
+            <Button
+              title="Salvar"
+              onPress={handleNew}
+            />
+          </View>
+        </ScrollView>
       </View>
-      
-    </KeyboardAvoidingView>
-    </>
+    </KeyboardAvoidingView >
   );
 }
--- Doctor-hu-hu/src/components/screens/Adicionar/style.ts
+++ Doctor-hu-hu/src/components/screens/Adicionar/style.ts
@@ -1,50 +1,19 @@
-import { colors } from "@/styles/colors";
-import { StyleSheet } from "react-native"; 
+import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-    fundo: {
-       
-        backgroundColor: colors.gray[700],
-        flex: 1, 
-       
-    },
-
-  
-    container:{
-        flex: 1,
-        marginTop: 50,
-       
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: '90%',
-        paddingBottom: 50,
-    },
-   input:{
-    backgroundColor: '#fff',
-    width: '90%',
-    marginBottom: 15,
-    color:'222',
-    fontSize: 17,
-    borderRadius: 7,
-    padding: 10,
-   },
-
-    btnSubmit:{
-     backgroundColor: colors.gray[300],
-     width: '90%',
-     height: 45,
-     alignItems: 'center',
-     justifyContent: 'center',
-     borderRadius: 7,
-    },
-    submitText:{
-        color: '#fff',
-        fontSize: 18,
-    },
-    btnRegister:{
-        marginTop: 10,
-    },
-    registerText:{
-        color: '#fff',
-    },
-})
+  container: {
+    backgroundColor: '#F2F3F5',
+    alignItems: 'center',
+  },
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  content: {
+    width: '100%',
+  },
+  footer: {
+    width: '100%',
+    padding: 24,
+  }
+});
